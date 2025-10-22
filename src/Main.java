1edd74a468a657f9afb83c930bea4c100116d240
AnteTolj <toljante20@gmail.com>
2025-10-22 18:21:23 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             suma += i;
             System.out.println("Broj po redu:" + i);
+            //ispisuje brojeve po redu
         }
         System.out.println("Suma brojeva je = " + suma);
     }
